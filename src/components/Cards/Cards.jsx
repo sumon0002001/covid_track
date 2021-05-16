7cc4d8059aadea6fef4591e7aaf04ecd19ecce91
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-17 02:56:33 +0600

    Add materials

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = (props) => {
-  console.log(props);
+import styles from './Cards.module.css';
+
+
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+  console.log(confirmed);
+  console.log(recovered);
+  console.log(deaths)
+
+  if (!confirmed) {
+    return 'Loading....'
+  }
   return (
-    <div>
-      <h2>Cards</h2>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card} >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+           <Typography variant="h5">
+             < CountUp
+             start={0}
+             end={confirmed.value}
+             duration={2.5}
+             separator=","
+             />
+             </Typography>
+             <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+            <Typography variant="body2">Number of Covid-19 Cases </Typography>
+          </CardContent>
+
+        </Grid>
+        <Grid item component={Card} >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5">REAL Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">Number of Recovered from Covid-19 Cases </Typography>
+          </CardContent>
+
+        </Grid>
+        <Grid item component={Card} >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Death</Typography>
+            <Typography variant="h5">REAL Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">Number of Death from Covid-19  </Typography>
+          </CardContent>
+
+        </Grid>
+
+      </Grid>
+
     </div>
   )
 };
