28a46abe96755786ce598a1a92a1172733df9e30
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-16 13:20:51 +0600

    Add api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from 'react';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import { fetchData } from './api';
 
 import styles from './App.module.css'
 
 // import { Cards, Chart, CountryPicker} from './components';
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
