b64d02324eea320ae4826af90269120a085ef363
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-18 23:46:47 +0600

    Bar chart

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
       <div className={styles.container}>
         < Cards data={data} />
         < CountryPicker handleCountryChange = {this.handleCountryChange} />
-        < Chart data ={data, country} />
+        <Chart data={data} country={country} /> 
       
       </div>
     )
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,19 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { fetchDailyData } from '../../api';
+import React, { useState, useEffect } from 'react';
 import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
 
 import styles from './Chart.module.css';
 
-const Chart = ({ data, country}) => {
-  const [ dailyData, setDailyData] = useState([]); 
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      setDailyData(await fetchDailyData());
-    }
-   
-    fetchAPI();
-  });
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+
+    fetchMyAPI();
+  }, []);
+
+  const barChart = (
+    confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${country}` },
+        }}
+      />
+    ) : null
+  );
 
   const lineChart = (
     dailyData[0] ? (
@@ -43,11 +67,12 @@ const Chart = ({ data, country}) => {
       />
     ) : null
   );
+
   return (
     <div className={styles.container}>
-      {lineChart}
+      {country ? barChart : lineChart}
     </div>
-  )
+  );
 };
 
 export default Chart;
